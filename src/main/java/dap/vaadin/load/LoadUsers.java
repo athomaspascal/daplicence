9b3cbd7c61286823a99c00561b41ff685363293f
pascalthomas <athomaspascal@gmail.com>
2018-09-04 00:01:21 +0200

    v5.3

--- src/main/java/dap/vaadin/load/LoadUsers.java
+++ src/main/java/dap/vaadin/load/LoadUsers.java
@@ -1,25 +1,95 @@
 package dap.vaadin.load;
 
+import dap.vaadin.crudui.app.JPAService;
+import dap.vaadin.crudui.entities.product.Product;
 import dap.vaadin.crudui.entities.user.User;
 import dap.vaadin.crudui.entities.user.UserRepository;
+import elastic.ElasticClient;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
 public class LoadUsers {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         LoadUsers loadUsers = new LoadUsers();
         loadUsers.init();
         List<User> all = UserRepository.findAll("");
+
+
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(
+                    new FileReader("C:\\Users\\pascalthomas\\IdeaProjects\\users-data.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
         for(User user : all){
             System.out.println(user.getDateCreation().toString() + ":" +
                     user.getTeamid().getNomteam() + ":" +
                     user.getTeamid().getUserDivision().getNameDivision() + ":" +
                     user.getNom());
+            Set<Product> products = user.getProducts();
+            try {
+                ElasticClient el1 = new ElasticClient();
+                Properties general = new Properties();
+                String fic = "general.properties";
+                general.load(Thread.currentThread().getContextClassLoader().getResourceAsStream(fic));
+                el1.connectElastic();
+                el1.client.prepareIndex(
+                        general.getProperty("indexName"),
+                        general.getProperty("indexType"),user.getId().toString())
+                        .setSource(jsonBuilder()
+                                .startObject()
+                                .field("name",user.getNom())
+                                .field("team", user.getTeamid().getNomteam())
+                                .field("division",user.getTeamid().getUserDivision().getNameDivision())
+                                .field("")
+                                .field("timestamp",user.getDateCreation())
+                                .endObject())
+                        .get();
+            }
+            catch(Exception e)
+            {
+                System.out.print("Erreur : "+ e.getMessage());
+                e.printStackTrace();
+            }
         }
         System.out.println("Fin");
-        
+        JPAService.close();
+
+    }
+
+    private static void updateUser(BufferedReader br, User user) {
+        try {
+
+            String thisLine = br.readLine();
+            StringTokenizer s = new StringTokenizer(thisLine,",");
+            s.nextToken();s.nextToken();
+            user.setNom(s.nextToken());
+
+
+            Date d = new Date();
+            Calendar cal = new GregorianCalendar();
+            cal.set(Calendar.DAY_OF_YEAR, 1);
+
+            long random = ThreadLocalRandom.current().nextLong(cal.getTime().getTime(), d.getTime());
+            Date date = new Date(random);
+            user.setDateCreation(date);
+            UserRepository.save(user);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private  EntityManagerFactory factory;
--- src/main/resources/create_database.sql
+++ src/main/resources/create_database.sql
@@ -1,63 +1,154 @@
+create schema PUBLIC
+;
+
 create table DIVISION
 (
-  ID            INTEGER default (NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_25F4E63E_7414_49F5_94AB_415490C60B9A)
-    primary key,
-  NAME_DIVISION VARCHAR(50) not null
-    constraint DIVISION_NAME_DIVISION_UINDEX
-    unique,
-  DESCRIPTION   VARCHAR(150)
-);
+	ID INTEGER default (NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_25F4E63E_7414_49F5_94AB_415490C60B9A)
+		primary key,
+	NAME_DIVISION VARCHAR(50) not null
+		constraint DIVISION_NAME_DIVISION_UINDEX
+			unique,
+	DESCRIPTION VARCHAR(150)
+)
+;
+
+create table EMPLOYEES
+(
+	EMP_NO INTEGER not null
+		primary key,
+	BIRTH_DATE DATE not null,
+	FIRST_NAME VARCHAR(14) not null,
+	LAST_NAME VARCHAR(16) not null,
+	GENDER ENUM(max) not null,
+	HIRE_DATE DATE not null
+)
+;
+
+create table ENVIRONNEMENT
+(
+	ID INTEGER default (NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_485B9D74_EF00_4B3D_8325_B39B04DF9300),
+	ENVIRONNEMENT_NAME VARCHAR(20)
+)
+;
+
+create unique index ENVIRONNEMENT_ID_UINDEX
+	on ENVIRONNEMENT (ID)
+;
+
+create unique index PRIMARY_KEY_8
+	on ENVIRONNEMENT (ID)
+;
+
+alter table ENVIRONNEMENT
+	add primary key (ID)
+;
 
 create table PRODUCT
 (
-  ID    BIGINT not null
-    primary key,
-  ADMIN BOOLEAN,
-  NAME  VARCHAR(255)
-);
+	ID BIGINT not null
+		primary key,
+	ADMIN BOOLEAN,
+	NAME VARCHAR(255)
+)
+;
 
 create table TEAM
 (
-  NOM_TEAM        VARCHAR(50),
-  ID              BIGINT default (NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_D63D3972_523D_4DEE_90A1_F0CCCF3DA54C)
-    primary key,
-  USERDIVISION_ID BIGINT,
-  constraint TEAM_DIVISION_ID_FK
-  foreign key (USERDIVISION_ID) references DIVISION
-);
+	NOMTEAM VARCHAR(50) not null,
+	ID BIGINT default (NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_A30D8C85_2312_402D_8EDE_8637CDFCE82F)
+		primary key,
+	USERDIVISION_ID BIGINT,
+	TEAMBOSSNAME VARCHAR(50),
+	TEAMBOSSEMAIL VARCHAR(50),
+	constraint FK18SAUD0G1WGP083F2WCW44T96
+		foreign key (USERDIVISION_ID) references DIVISION,
+	constraint TEAM_DIVISION_ID_FK
+		foreign key (USERDIVISION_ID) references DIVISION
+)
+;
+
+create table CONTACTS
+(
+	ID BIGINT default (NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_1EE4ABA3_4A3C_4506_8881_94BD5F584148)
+		primary key,
+	NAME VARCHAR(50),
+	CONTACT_EMAIL VARCHAR(50),
+	TEAM_ID BIGINT not null,
+	constraint CONTACTS_TEAM_ID_FK
+		foreign key (TEAM_ID) references TEAM
+)
+;
+
+create table TEAM_COPY_4_0
+(
+	NOMTEAM VARCHAR(50) not null,
+	ID BIGINT default (NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_086A6B53_29CF_4943_BF68_E6D8E6AD1940)
+		primary key,
+	USERDIVISION_ID BIGINT,
+	TEAMBOSSNAME VARCHAR(50),
+	TEAMBOSSEMAIL VARCHAR(50)
+)
+;
+
+create table TEAM_COPY_6_1
+(
+	NOMTEAM VARCHAR(50) not null,
+	ID BIGINT default (NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_BD1185D9_1D54_44AE_8651_7F105508D7DD)
+		primary key,
+	USERDIVISION_ID BIGINT,
+	TEAMBOSSNAME VARCHAR(50),
+	TEAMBOSSEMAIL VARCHAR(50)
+)
+;
 
 create table USER
 (
-  ID             BIGINT       not null
-    primary key,
-  NOM            VARCHAR(255) not null,
-  ACTIVE         BOOLEAN,
-  DATECREATION   TIMESTAMP,
-  EMAIL          VARCHAR(255) not null,
-  GENDER         INTEGER,
-  MATRICULE      INTEGER      not null,
-  PASSWORD       VARCHAR(100) not null,
-  MAINPRODUCT_ID BIGINT,
-  TEAMID_ID      BIGINT,
-  constraint FK8GA285XNNMH27TUXWDWHQMMNP
-  foreign key (TEAMID_ID) references TEAM,
-  constraint FKEPSVYLIC4X63C14ISTDNV0D2P
-  foreign key (TEAMID_ID) references TEAM,
-  constraint FKOYT5LTRM670TRXVXFGS7FJTVB
-  foreign key (TEAMID_ID) references TEAM,
-  constraint USER_TEAM_ID_FK
-  foreign key (TEAMID_ID) references TEAM
-);
+	ID BIGINT not null
+		primary key,
+	NOM VARCHAR(255) not null,
+	ACTIVE BOOLEAN,
+	DATECREATION TIMESTAMP,
+	EMAIL VARCHAR(255) not null,
+	GENDER INTEGER,
+	MATRICULE INTEGER not null,
+	PASSWORD VARCHAR(100) not null,
+	MAINPRODUCT_ID BIGINT,
+	TEAMID_ID BIGINT,
+	constraint FK8GA285XNNMH27TUXWDWHQMMNP
+		foreign key (TEAMID_ID) references TEAM,
+	constraint FKCO3GFS3N2QEVRS4UG842KQKW2
+		foreign key (MAINPRODUCT_ID) references PRODUCT,
+	constraint FKEPSVYLIC4X63C14ISTDNV0D2P
+		foreign key (TEAMID_ID) references TEAM,
+	constraint FKOYT5LTRM670TRXVXFGS7FJTVB
+		foreign key (TEAMID_ID) references TEAM,
+	constraint USER_TEAM_ID_FK
+		foreign key (TEAMID_ID) references TEAM
+)
+;
+
+create table USER_ENVIRONNEMENT
+(
+	USER_ID BIGINT not null,
+	ENVIRONNEMENTS_ID BIGINT not null,
+	constraint USER_ENVIRONNEMENT_PK_2
+		primary key (USER_ID, ENVIRONNEMENTS_ID),
+	constraint USER_ENVIRONNEMENT_ENVIRONNEMENT_ID_FK
+		foreign key (ENVIRONNEMENTS_ID) references ENVIRONNEMENT,
+	constraint USER_ENVIRONNEMENT_USER_ID_FK
+		foreign key (USER_ID) references USER
+)
+;
 
 create table USER_PRODUCT
 (
-  USER_ID     BIGINT not null,
-  PRODUCTS_ID BIGINT not null,
-  primary key (USER_ID, PRODUCTS_ID),
-  constraint FKBT8RTEPMVTSBHBJCS6ADUI5BY
-  foreign key (USER_ID) references USER,
-  constraint FKIO1UHULOD679XJAYF81CNVNCO
-  foreign key (PRODUCTS_ID) references PRODUCT
-);
+	USER_ID BIGINT not null,
+	PRODUCTS_ID BIGINT not null,
+	primary key (USER_ID, PRODUCTS_ID),
+	constraint FKBT8RTEPMVTSBHBJCS6ADUI5BY
+		foreign key (USER_ID) references USER,
+	constraint FKIO1UHULOD679XJAYF81CNVNCO
+		foreign key (PRODUCTS_ID) references PRODUCT
+)
+;
 
-t
